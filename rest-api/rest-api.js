adcb8bcdaea9e716add0a35ef394ffca6462f2fe
Nico Braun <nico.braun@concentrix.com>
2020-08-27 12:11:35 +0100

    better client example html

--- rest-api/rest-api.js
+++ rest-api/rest-api.js
@@ -14,11 +14,18 @@ fastify.register(require('fastify-jwt'), {
 fastify.get("/authenticate", async function (req, reply) {
   try {
     await fastify.jwt.verify(req.query.token)
-    const payload = {username: fastify.jwt.decode(req.query.token).username, role: 'admin'}
-    reply.send({token: fastify.jwt.sign(payload, {expiresIn: '1d'})})
+    const payload = { username: fastify.jwt.decode(req.query.token).username, role: 'admin' }
+    reply.send({ token: fastify.jwt.sign(payload, { expiresIn: '1m' }) })
   } catch (err) {
     reply.send(err)
   }
+})
+
+fastify.get("/login", async (req, reply) => {
+  const token = fastify.jwt.sign({ redirect: 'http://localhost:5000/authenticate' }, {
+    expiresIn: '30s'
+  })
+  reply.redirect('http://localhost:4000/login?token=' + token)
 })
 
 fastify.register((fastify, options, done) => {
@@ -26,10 +33,6 @@ fastify.register((fastify, options, done) => {
     try {
       await req.jwtVerify()
     } catch (err) {
-      const token = fastify.jwt.sign({ redirect: 'http://localhost:5000/authenticate' }, {
-        expiresIn: '30s'
-      })
-      reply.redirect('http://localhost:4000/login?token=' + token)
       reply.send(err)
     }
   })
